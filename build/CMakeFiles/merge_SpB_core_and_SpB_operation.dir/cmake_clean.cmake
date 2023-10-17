c67e51bbbf818ad3d33c99242d45f68f909661d8
wang-luhan <iswanglh@outlook.com>
2023-10-17 22:24:02 +0800

    remove some makefile

--- build/CMakeFiles/merge_SpB_core_and_SpB_operation.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/merge_SpB_core_and_SpB_operation"
-  "libopensparseblas.a"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/merge_SpB_core_and_SpB_operation.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/merge_SpB_core_and_SpB_operation_so.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for merge_SpB_core_and_SpB_operation_so.
-# This may be replaced when dependencies are built.
